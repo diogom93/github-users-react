a4526a833bc01c88b1c42e2561e5cde26ec984ec
Diogo Martins <nunodiogomartins@gmail.com>
2019-10-06 21:59:05 +0100

    Finished first version of app
    Create Form component to search for a given username.
    
    Add Form and CardList components to App.
    
    Small change of style in CardList component.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Card from './Card.js';
+import Card from './Card';
 
 const CardList = (props) => {
-    return <div>
-        {props.cards.map(card => (
-            <Card {...card}/>
-        ))}
-    </div>
+    return (
+        <div>
+            {props.cards.map(card => (
+                <Card {...card}/>
+            ))}
+        </div>
+    );
 }
 
 export default CardList;
